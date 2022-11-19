70aad965695cad7a488727ebca860395d474ea8e
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-11-19 19:39:25 +0000

    Implemented login and signup

--- project/auth.py
+++ project/auth.py
@@ -1,18 +1,52 @@
-from flask import Blueprint
-from flask import Flask
-from flask import request, render_template, session
-from flask_bootstrap import Bootstrap5
+from flask import Blueprint, redirect, url_for, request, render_template, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template('login.html', title='Home')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+    
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+
+    return redirect(url_for('main.profile'))
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    # Login code goes here
+    return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
 def signup():
     return render_template('signup.html', title='Home')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    # Check if user already exists
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        flash('Email address already exists.')
+        return redirect(url_for('auth.signup'))
+    
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
