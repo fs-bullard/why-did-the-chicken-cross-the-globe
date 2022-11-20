f3fd6d814bee33d187c9f04230d76a0ecd58adbd
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-11-20 00:58:09 +0000

    Added carbon object

--- project/models.py
+++ project/models.py
@@ -6,3 +6,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    carbon_totals = db.relationship('Carbon_Total', backref='user', lazy=True)
+
+class Carbon_Total(UserMixin, db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Integer)
+    value = db.Column(db.Integer)
