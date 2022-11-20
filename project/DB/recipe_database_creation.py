2e2e3a20dd5779b1a52825fd7b35d76c7f125fc1
Boingzns <33784478+Boingzns@users.noreply.github.com>
2022-11-20 01:03:04 +0000

    bunch o stuff

--- project/DB/recipe_database_creation.py
+++ project/DB/recipe_database_creation.py
@@ -2,50 +2,10 @@ import json
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-
-category_map = {
-    'Apples': ['apples', 'apple'],
-    'Bananas': ['bananas', 'banana'],
-    'Barley': ['barley'],
-    'Beef (beef herd)': ['beef (beef herd), beef', 'venison'],
-    'Berries & Grapes': ['berries & grapes, berry', 'grape'],
-    'Brassicas': ['brassicas', 'brassica'],
-    'Cane Sugar': ['cane sugar', 'sugar'],
-    'Cassava': ['cassava'],
-    'Cheese': ['cheese', 'mascarpone', 'cheddar', 'mozzarella', 'parmesan', 'ricotta'],
-    'Citrus Fruit': ['citrus fruit', 'lemon'],
-    'Coffee': ['coffee'],
-    'Dark Chocolate': ['dark chocolate', 'chocolate'],
-    'Eggs': ['eggs', 'egg'],
-    'Fish (farmed)': ['fish (farmed)', 'fish', 'cod', 'anchovy', 'salmon'],
-    'Groundnuts': ['groundnuts', 'nut', 'nutmeg', 'almond', 'pine'],
-    'Lamb & Mutton': ['lamb & mutton', 'lamb', 'mutton'],
-    'Maize': ['maize', 'corn'],
-    'Milk': ['milk', 'butter', 'yoghurt', 'crème', 'creme', 'cream'],
-    'Oatmeal': ['oatmeal', 'oat', 'oats'],
-    'Onions & Leeks': ['onions & leeks', 'onion', 'leak', 'garlic', 'shallot'],
-    'Other Fruit': ['other fruit', 'fruit', 'fruits', 'pineapple', 'cucumber', 'cornichon'],
-    'Other Pulses': ['other pulses', 'pulse', 'pulses', 'lentils', 'bean', 
-        'green olive', 'caper', 'chickpea'],
-    'Other Vegetables': ['other vegetables', 'vegtable', 'vegtables', 'oil', 'peppers',
-         'aubergine', 'kale', 'salad', 'chipotle', 'mushroom', 'spinach', 
-         'basil', 'lettuce', 'broccoli', 'avocado', 'cabbage', 'carrot'],
-    'Peas': ['peas', 'pea'],
-    'Pig Meat': ['pig meat', 'pig', 'pork', 'bacon', 'chorizo', 'ham', 'pancetta'],
-    'Potatoes': ['potatoes', 'potato', 'gnocchi'],
-    'Poultry Meat': ['poultry meat', 'poultry', 'chicken',  'turkey'],
-    'Prawns (farmed)': ['prawns (farmed)', 'prawn', 'prawns'],
-    'Rice': ['rice', 'couscous'],
-    'Root Vegetables': ['root vegetables', 'root vegetable'],
-    'Soy milk': ['soy milk', 'soy'],
-    'Tofu': ['tofu'],
-    'Tomatoes': ['tomatoes', 'tomato', 'ketchup'],
-    'Wheat & Rye': ['wheat & rye', 'bread', 'pasta', 'penne', 'lasagne sheet', 
-        'flour', 'breadcrumb', 'pastry', 'fusilli', 'macaroni'],
-    'Wine': ['wine'],
-    }
+from variables import category_map
 
 recipe_database = []
+all_lists_database = []
 collection = requests.get("https://www.bbcgoodfood.com/recipes/collection/easy-dinner-recipes")
 soup = BeautifulSoup(collection.content, 'html.parser')
 links = soup.find_all("a",{"class":"link d-block"})
@@ -91,13 +51,29 @@ for link in links:
                      'tags': tags,
                      'nutrition_info': nutrition_info,
                      'serves': int(serves),
-                     'tags': tags,
                      'ingredients': ingredients,
                      'categories': categories,
                      'quantities': quantities})
 
+    for i in range(len(ingredients)):
+        all_lists_database.append({'title': title,
+                        'description': description,
+                        'url': url,
+                        'tags': tags,
+                        'nutrition_info': nutrition_info,
+                        'serves': int(serves),
+                        'ingredients': ingredients[i],
+                        'categories': categories[i],
+                        'quantities': quantities[i]})
 
-df = pd.DataFrame.from_dict(recipe_database)
-df.to_excel('project/DB/recipe_database.xlsx')
+
+df1 = pd.DataFrame.from_dict(recipe_database)
+df1.to_excel('project/DB/recipe_database.xlsx')
+
+df2 = pd.DataFrame.from_dict(all_lists_database)
+df2.to_excel('project/DB/all_list_database.xlsx')
+
+
+
                      
 
