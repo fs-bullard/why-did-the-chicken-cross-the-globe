9aff58fed3e3ad3bcd24e3424de7ce7279c72a70
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-11-20 02:57:35 +0000

    got rid of shopping list and ingredient db

--- project/models.py
+++ project/models.py
@@ -6,9 +6,24 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    # shopping_lists = db.relationship('Shopping_List', backref='user', lazy=True)
     carbon_totals = db.relationship('Carbon_Total', backref='user', lazy=True)
 
-class Carbon_Total(UserMixin, db.model):
+# class Shopping_List(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     date = db.Column(db.Date, nullable=False)
+#     ingredients = db.relationship('Ingredient', backref='shopping_list', lazy=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+# class Ingredient(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100))
+#     shopping_list_id = db.Column(db.Integer, db.ForeignKey('shopping_list.id'), nullable=False)
+
+class Carbon_Total(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Integer)
+    date = db.Column(db.Integer, nullable=False)
     value = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
